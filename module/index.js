044c55b21e892bb6fd339ebacff2ee04baf59627
hanbinchoi <wkghskak@naver.com>
2021-11-23 21:38:16 +0900

    first commit

---
+++ module/index.js
@@ -0,0 +1,7 @@
+// -- commonJS
+// const { getCircleArea } = require('./mathUtil');
+
+// -- ESM
+import { getCircleArea } from './mathUtil';
+const result = getCircleArea(2);
+console.log(result);
---
+++ module/mathUtil.js
@@ -0,0 +1,16 @@
+const PI = 3.14;
+const getCircleArea = (r) => r * r * PI;
+
+// -- commonJS
+
+// module.exports = {
+//   PI,
+//   getCircleArea,
+// };
+
+// exports.PI = PI;
+
+// exports.getCircleArea = getCircleArea;
+
+// -- ESM
+export { PI, getCircleArea };
